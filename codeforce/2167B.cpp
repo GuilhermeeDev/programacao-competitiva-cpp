259e16ee05a2cc9d52a1dc42f6065118ea716fdf
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-26 15:37:46 -0400

    (add) Lista de Exercicio 2167 - Ex A Codeforce.

---
+++ codeforce/2167B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 0; i<n; i++){
+        int q;
+        cin >> q;
+        string s,t;
+        cin >> s >> t;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if (s == t) cout << "YES" << endl;
+        else cout << "NO" << endl; 
+    }
+}
