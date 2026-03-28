531d79883b60010c2be54f59a3e14e6fb3c5e2a2
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-28 19:46:54 -0400

    (add) Exercicio 1046 do BeeCrowd.

---
+++ beecrowd/1046.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if (a == b) cout << "O JOGO DUROU 24 HORA(S)" << endl;
+    else if (b>a) cout << "O JOGO DUROU " << b-a << " HORA(S)" << endl;
+    else cout << "O JOGO DUROU " << abs((a-b)-24) << " HORA(S)" << endl;
+    return 0;
+}
