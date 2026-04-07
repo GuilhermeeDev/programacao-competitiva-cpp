fcf546807aa1890f51896e55957e512d3de582bf
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-04-07 14:05:55 -0300

    (add) Exercicio 3432 do BeeCrowd.

---
+++ beecrowd/3432.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1,n2,n3,n4,n5,n6,n7,n8;
+    cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7 >> n8;
+
+    if (n1 == 9 || n2 == 9 || n3 == 9 || n4 == 9 || n5 == 9 || n6 == 9 || n7 == 9 || n8 == 9) cout << "F\n";
+    else cout << "S\n";
+}
