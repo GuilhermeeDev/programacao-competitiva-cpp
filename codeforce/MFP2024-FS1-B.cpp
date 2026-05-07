fb3fa24c3caa9ae517d163f24a8a2f9dbd4f102d
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-05-06 22:34:13 -0400

    (add) Exercicio MFP 2025 primera fase - B

---
+++ codeforce/MFP2024-FS1-B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x1, x2, x3 ,x4, y1, y2, y3, y4;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    cin >> x3 >> y3;
+    cin >> x4 >> y4;
+
+    
+
+    return 0;
+}
